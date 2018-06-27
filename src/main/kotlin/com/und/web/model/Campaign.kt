de68e929191c3a2dc1697fd54fc334de27d9802c
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-06-27 14:25:38 +0530

    Integration testing

---
+++ src/main/kotlin/com/und/web/model/Campaign.kt
@@ -0,0 +1,20 @@
+
+import com.und.model.CampaignStatus
+import com.und.model.jpa.CampaignType
+import com.und.model.jpa.Schedule
+import java.time.LocalDateTime
+
+
+class CampaignWeb {
+    var id: Long? = null
+    var name: String = ""
+    var schedule: Schedule? = null
+    var campaignType: CampaignType? = null
+    var segmentationID: Long? = null
+    var templateID: Long? = null
+    var status: CampaignStatus? = null
+    var dateCreated: LocalDateTime? = null
+    var dateModified: LocalDateTime? = null
+
+}
+
---
+++ src/main/kotlin/com/und/web/model/ContactUs.kt
@@ -0,0 +1,4 @@
+package com.und.web.model
+
+class ContactUs {
+}
